f2e491b109be7adfb510a03c7f30d3f6d1c57b40
dayr1018 <dayr1018@naver.com>
2022-06-14 14:15:38 +0900

    added point-cloud file creator

---
+++ metadata/MakeTextFileCode_RGB_Depth_PointCloud/MakeDataFile_RGB_Depth_PointCloud.py
@@ -0,0 +1,53 @@
+# Train file, Test file 두 가지가 필요함 
+# Root 는 /mnt/nas3/yrkim/liveness_lidar_project/GC_project/LDFAS 임 
+# 데이터는 총 1~12번까지 있으며, Test 데이터의 경우는 내가 임의로 추가해줘야 함 
+
+Train_File = "train_data.txt"
+Test_File = "test_data.txt"
+
+Train_Start = 1
+Train_End = 6 # means 5
+Test_Start = 6
+Test_End = 11 # means 10
+
+facedata_start = 1
+facedata_end = 21 # means 20
+
+with open(Train_File, 'w') as file:
+    for subNum in range(Train_Start,Train_End):
+        for fileNum in range(facedata_start, facedata_end):
+            file.write(f"LDFAS/{subNum}/bonafide/rgb_{fileNum}.jpg LDFAS/{subNum}/bonafide/pc_{fileNum}.ply 1\n")
+        for fileNum in range(facedata_start, facedata_end):
+            file.write(f"LDFAS/{subNum}/attack_mask/rgb_{fileNum}.jpg LDFAS/{subNum}/attack_mask/pc_{fileNum}.ply 0\n")
+        # for fileNum in range(facedata_start, facedata_end):
+            #file.write(f"LDFAS/{subNum}/attack_paper/rgb_{fileNum}.jpg LDFAS/{subNum}/attack_paper/pc_{fileNum}.ply 0\n")
+    
+    # for subNum in range(Train_Start,Train_End):
+    #     for fileNum in range(facedata_start, facedata_end):
+            
+    # for subNum in range(Train_Start,Train_End):
+    #     for fileNum in range(facedata_start, facedata_end):
+    #         
+
+with open(Test_File, 'w') as file:
+    for subNum in range(Test_Start,Test_End):
+        for fileNum in range(facedata_start, facedata_end):
+            file.write(f"LDFAS/{subNum}/bonafide/rgb_{fileNum}.jpg LDFAS/{subNum}/bonafide/pc_{fileNum}.ply 1\n")
+        for fileNum in range(facedata_start, facedata_end):
+            file.write(f"LDFAS/{subNum}/attack_mask/rgb_{fileNum}.jpg LDFAS/{subNum}/attack_mask/pc_{fileNum}.ply 0\n")
+        # for fileNum in range(facedata_start, facedata_end):
+            #file.write(f"LDFAS/{subNum}/attack_paper/rgb_{fileNum}.jpg LDFAS/{subNum}/attack_paper/pc_{fileNum}.ply 0\n")
+    # for subNum in range(Test_Start,Test_End):
+    #     for fileNum in range(facedata_start, facedata_end):
+    #         file.write(f"LDFAS/{subNum}/attack_mask/rgb_{fileNum}.jpg LDFAS/{subNum}/attack_mask/pc_{fileNum}.ply 0\n")
+    # for subNum in range(Test_Start,Test_End):
+    #     for fileNum in range(facedata_start, facedata_end):
+    #         file.write(f"LDFAS/{subNum}/attack_paper/rgb_{fileNum}.jpg LDFAS/{subNum}/attack_paper/pc_{fileNum}.ply 0\n")
+
+files = [Train_File, Test_File]
+
+for filename in files:
+    file = open(filename, 'r')
+    print(filename)
+    print(file.read().count("\n"))
+    file.close()
