ad3d6a37c5ca9c709346cdfdca685191c4eaf7b1
YongraeKim <dayr1018@gmail.com>
2022-07-26 17:24:38 +0900

    modified npy generator for outdoor/indoor_dark data

--- npy_generator_for_ply.py
+++ npy_generator_for_ply.py
@@ -29,138 +29,154 @@ if __name__ == "__main__":
     data_path = '/mnt/nas3/yrkim/liveness_lidar_project/GC_project/LDFAS'
     save_path = '/mnt/nas3/yrkim/liveness_lidar_project/GC_project/LDFAS/NPY_Files'
 
-    person_number = [int(i) for i in os.listdir(data_path) if i.isdigit()]
+    kind = ["1. Indoor", "2. Outdoor", "3. Indoor_dark"]
+    for idx, dir_name in enumerate(kind):
+        dir_path = osp.join(data_path, dir_name)
+        
+        person_number = [int(i) for i in os.listdir(dir_path) if i.isdigit()]
+        for i in person_number:
+            if not osp.exists(osp.join(save_path, dir_name, 'real_cloud_data', str(i))) :
+                os.makedirs(osp.join(save_path, dir_name, 'real_cloud_data', str(i)))
+            if not osp.exists(osp.join(save_path, dir_name, 'mask_cloud_data', str(i))) :
+                os.makedirs(osp.join(save_path, dir_name, 'mask_cloud_data', str(i)))
+            if not osp.exists(osp.join(save_path, dir_name, 'replay_cloud_data', str(i))) :
+                os.makedirs(osp.join(save_path, dir_name, 'replay_cloud_data', str(i)))
+            if not osp.exists(osp.join(save_path, dir_name, 'paper_cloud_data', str(i))) :
+                os.makedirs(osp.join(save_path, dir_name, 'paper_cloud_data', str(i)))
     
-    for i in person_number :
-        if not osp.exists(osp.join(save_path, 'real_cloud_data',str(i))) :
-            os.makedirs(osp.join(save_path, 'real_cloud_data',str(i)))
-    for i in person_number :
-        if not osp.exists(osp.join(save_path, 'mask_cloud_data',str(i))) :
-            os.makedirs(osp.join(save_path, 'mask_cloud_data',str(i)))
-    for i in person_number :
-        if not osp.exists(osp.join(save_path, 'replay_cloud_data',str(i))) :
-            os.makedirs(osp.join(save_path, 'replay_cloud_data',str(i)))
-    for i in person_number :
-        if not osp.exists(osp.join(save_path, 'paper_cloud_data',str(i))) :
-            os.makedirs(osp.join(save_path, 'paper_cloud_data',str(i)))
-
+  
+# 여기까지 함 
+                        
+            
     # bonafide
     for i in tqdm(person_number) :
-        num_path = osp.join(data_path,str(i))
-        files = os.listdir(osp.join(num_path,'bonafide'))
-        files = [j for j in files if j.split('_')[0]=='pc']
-        for j in files : 
-            ply = osp.join(osp.join(num_path,'bonafide',j))
-            real = read_ply_xyzrgbnormal(ply)
-    #         proj_depth = np.full((256, 192), -1,dtype=np.float32)
-    #         proj_xyz = np.full((3, 256, 192), -1,dtype=np.float32)
-            proj_xyzd = np.full((4, 192, 256), -1,dtype=np.float32)
-            for k in range(len(real)):
-    #             proj_depth[int(real[k, 3]), int(real[k, 4])] = real[k, 5]
-                proj_xyzd[0, int(real[k, 4]), int(real[k, 3])] = real[k, 0]
-                proj_xyzd[1, int(real[k, 4]), int(real[k, 3])] = real[k, 1]
-                proj_xyzd[2, int(real[k, 4]), int(real[k, 3])] = real[k, 2]
-                proj_xyzd[3, int(real[k, 4]), int(real[k, 3])] = real[k, 5]
-            proj_xyzd = proj_xyzd.transpose(1,2,0)
-            
-            save_name = osp.join(save_path, 'real_cloud_data',str(i),j.split('.')[0]+'.npy')
-            np.save(save_name, proj_xyzd)
+        for dir_name in kind:
+            num_path = osp.join(data_path, dir_name, str(i))
+            files = os.listdir(osp.join(num_path,'bonafide'))
+            files = [j for j in files if j.split('_')[0]=='pc']
+            for j in files : 
+                ply = osp.join(osp.join(num_path,'bonafide',j))
+                real = read_ply_xyzrgbnormal(ply)
+        #         proj_depth = np.full((256, 192), -1,dtype=np.float32)
+        #         proj_xyz = np.full((3, 256, 192), -1,dtype=np.float32)
+                proj_xyzd = np.full((4, 192, 256), -1,dtype=np.float32)
+                for k in range(len(real)):
+        #             proj_depth[int(real[k, 3]), int(real[k, 4])] = real[k, 5]
+                    proj_xyzd[0, int(real[k, 4]), int(real[k, 3])] = real[k, 0]
+                    proj_xyzd[1, int(real[k, 4]), int(real[k, 3])] = real[k, 1]
+                    proj_xyzd[2, int(real[k, 4]), int(real[k, 3])] = real[k, 2]
+                    proj_xyzd[3, int(real[k, 4]), int(real[k, 3])] = real[k, 5]
+                proj_xyzd = proj_xyzd.transpose(1,2,0)
+
+                save_name = osp.join(save_path, dir_name, 'real_cloud_data',str(i),j.split('.')[0]+'.npy')
+                np.save(save_name, proj_xyzd)
 
     # data check
-    for i in person_number :
-        org_path = osp.join(data_path,str(i))
-        org_files = os.listdir(osp.join(num_path,'bonafide'))
-        org_files = [j for j in org_files if j.split('_')[0]=='pc']
-        
-        new_path = osp.join(save_path, 'real_cloud_data', str(i))
-        new_files = os.listdir(new_path)
-        print(i,f' Org Data Count : {len(org_files)}  New Data Count : {len(new_files)} ==> {len(org_files)==len(new_files)}')
-        
+    for dir_name in kind:
+        for i in person_number :
+            # org_path = osp.join(data_path,str(i))
+            num_path = osp.join(data_path, dir_name, str(i))
+            org_files = os.listdir(osp.join(num_path,'bonafide'))
+            org_files = [j for j in org_files if j.split('_')[0]=='pc']
+
+            new_path = osp.join(save_path, dir_name, 'real_cloud_data', str(i))
+            new_files = os.listdir(new_path)
+            print(i,f' Org Data Count : {len(org_files)}  New Data Count : {len(new_files)} ==> {len(org_files)==len(new_files)}')
+
+            
     # attack_mask        
     for i in tqdm(person_number) :
-        num_path = osp.join(data_path,str(i))
-        files = os.listdir(osp.join(num_path,'attack_mask'))
-        files = [j for j in files if j.split('_')[0]=='pc']
-        for j in files : 
-            ply = osp.join(osp.join(num_path,'attack_mask',j))
-            real = read_ply_xyzrgbnormal(ply)
-            proj_xyzd = np.full((4, 192, 256), -1,dtype=np.float32)
-            for k in range(len(real)):
-                proj_xyzd[0, int(real[k, 4]), int(real[k, 3])] = real[k, 0]
-                proj_xyzd[1, int(real[k, 4]), int(real[k, 3])] = real[k, 1]
-                proj_xyzd[2, int(real[k, 4]), int(real[k, 3])] = real[k, 2]
-                proj_xyzd[3, int(real[k, 4]), int(real[k, 3])] = real[k, 5]
-            proj_xyzd = proj_xyzd.transpose(1,2,0)
-            
-            save_name = osp.join(save_path, 'mask_cloud_data',str(i),j.split('.')[0]+'.npy')
-            np.save(save_name, proj_xyzd)
+        for dir_name in kind:
+            num_path = osp.join(data_path, dir_name, str(i))
+            files = os.listdir(osp.join(num_path,'attack_mask'))
+            files = [j for j in files if j.split('_')[0]=='pc']
+            for j in files : 
+                ply = osp.join(osp.join(num_path,'attack_mask',j))
+                real = read_ply_xyzrgbnormal(ply)
+                proj_xyzd = np.full((4, 192, 256), -1,dtype=np.float32)
+                for k in range(len(real)):
+                    proj_xyzd[0, int(real[k, 4]), int(real[k, 3])] = real[k, 0]
+                    proj_xyzd[1, int(real[k, 4]), int(real[k, 3])] = real[k, 1]
+                    proj_xyzd[2, int(real[k, 4]), int(real[k, 3])] = real[k, 2]
+                    proj_xyzd[3, int(real[k, 4]), int(real[k, 3])] = real[k, 5]
+                proj_xyzd = proj_xyzd.transpose(1,2,0)
+
+                save_name = osp.join(save_path, dir_name, 'mask_cloud_data',str(i),j.split('.')[0]+'.npy')
+                np.save(save_name, proj_xyzd)
         
     # data check
-    for i in person_number :
-        org_path = osp.join(data_path,str(i))
-        org_files = os.listdir(osp.join(num_path,'attack_mask'))
-        org_files = [j for j in org_files if j.split('_')[0]=='pc']
-        
-        new_path = osp.join(save_path, 'mask_cloud_data', str(i))
-        new_files = os.listdir(new_path)
-        print(i,f' Org Data Count : {len(org_files)}  New Data Count : {len(new_files)} ==> {len(org_files)==len(new_files)}')
-        
+    for dir_name in kind:
+        for i in person_number :
+            # org_path = osp.join(data_path,str(i))
+            num_path = osp.join(data_path, dir_name, str(i))
+            org_files = os.listdir(osp.join(num_path,'attack_mask'))
+            org_files = [j for j in org_files if j.split('_')[0]=='pc']
+
+            new_path = osp.join(save_path, dir_name, 'mask_cloud_data', str(i))
+            new_files = os.listdir(new_path)
+            print(i,f' Org Data Count : {len(org_files)}  New Data Count : {len(new_files)} ==> {len(org_files)==len(new_files)}')
+
     # attack_replay
     for i in tqdm(person_number) :
-        num_path = osp.join(data_path,str(i))
-        files = os.listdir(osp.join(num_path,'attack_replay'))
-        files = [j for j in files if j.split('_')[0]=='pc']
-        for j in files : 
-            ply = osp.join(osp.join(num_path,'attack_replay',j))
-            real = read_ply_xyzrgbnormal(ply)
-            proj_xyzd = np.full((4, 192, 256), -1,dtype=np.float32)
-            for k in range(len(real)):
-                proj_xyzd[0, int(real[k, 4]), int(real[k, 3])] = real[k, 0]
-                proj_xyzd[1, int(real[k, 4]), int(real[k, 3])] = real[k, 1]
-                proj_xyzd[2, int(real[k, 4]), int(real[k, 3])] = real[k, 2]
-                proj_xyzd[3, int(real[k, 4]), int(real[k, 3])] = real[k, 5]
-            proj_xyzd = proj_xyzd.transpose(1,2,0)
-            
-            save_name = osp.join(save_path, 'replay_cloud_data',str(i),j.split('.')[0]+'.npy')
-            np.save(save_name, proj_xyzd)
+        for dir_name in kind:
+            num_path = osp.join(data_path, dir_name, str(i))
+            files = os.listdir(osp.join(num_path,'attack_replay'))
+            files = [j for j in files if j.split('_')[0]=='pc']
+            for j in files : 
+                ply = osp.join(osp.join(num_path,'attack_replay',j))
+                real = read_ply_xyzrgbnormal(ply)
+                proj_xyzd = np.full((4, 192, 256), -1,dtype=np.float32)
+                for k in range(len(real)):
+                    proj_xyzd[0, int(real[k, 4]), int(real[k, 3])] = real[k, 0]
+                    proj_xyzd[1, int(real[k, 4]), int(real[k, 3])] = real[k, 1]
+                    proj_xyzd[2, int(real[k, 4]), int(real[k, 3])] = real[k, 2]
+                    proj_xyzd[3, int(real[k, 4]), int(real[k, 3])] = real[k, 5]
+                proj_xyzd = proj_xyzd.transpose(1,2,0)
+
+                save_name = osp.join(save_path, dir_name, 'replay_cloud_data',str(i),j.split('.')[0]+'.npy')
+                np.save(save_name, proj_xyzd)
 
     # data check
-    for i in person_number :
-        org_path = osp.join(data_path,str(i))
-        org_files = os.listdir(osp.join(num_path,'attack_replay'))
-        org_files = [j for j in org_files if j.split('_')[0]=='pc']
-        
-        new_path = osp.join(save_path, 'replay_cloud_data', str(i))
-        new_files = os.listdir(new_path)
-        print(i,f' Org Data Count : {len(org_files)}  New Data Count : {len(new_files)} ==> {len(org_files)==len(new_files)}')
-        
+    for dir_name in kind:
+        for i in person_number :
+            # org_path = osp.join(data_path,str(i))
+            num_path = osp.join(data_path, dir_name, str(i))
+            org_files = os.listdir(osp.join(num_path,'attack_replay'))
+            org_files = [j for j in org_files if j.split('_')[0]=='pc']
+
+            new_path = osp.join(save_path, dir_name, 'replay_cloud_data', str(i))
+            new_files = os.listdir(new_path)
+            print(i,f' Org Data Count : {len(org_files)}  New Data Count : {len(new_files)} ==> {len(org_files)==len(new_files)}')
+
     # attack_paper
     for i in tqdm(person_number) :
-        num_path = osp.join(data_path,str(i))
-        files = os.listdir(osp.join(num_path,'attack_paper'))
-        files = [j for j in files if j.split('_')[0]=='pc']
-        for j in files : 
-            ply = osp.join(osp.join(num_path,'attack_paper',j))
-            real = read_ply_xyzrgbnormal(ply)
-            proj_xyzd = np.full((4, 192, 256), -1,dtype=np.float32)
-            for k in range(len(real)):
-                proj_xyzd[0, int(real[k, 4]), int(real[k, 3])] = real[k, 0]
-                proj_xyzd[1, int(real[k, 4]), int(real[k, 3])] = real[k, 1]
-                proj_xyzd[2, int(real[k, 4]), int(real[k, 3])] = real[k, 2]
-                proj_xyzd[3, int(real[k, 4]), int(real[k, 3])] = real[k, 5]
-            proj_xyzd = proj_xyzd.transpose(1,2,0)
-            
-            save_name = osp.join(save_path, 'paper_cloud_data', str(i),j.split('.')[0]+'.npy')
-            np.save(save_name, proj_xyzd)
+        for dir_name in kind:
+            num_path = osp.join(data_path, dir_name, str(i))
+            files = os.listdir(osp.join(num_path,'attack_paper'))
+            files = [j for j in files if j.split('_')[0]=='pc']
+            for j in files : 
+                ply = osp.join(osp.join(num_path,'attack_paper',j))
+                real = read_ply_xyzrgbnormal(ply)
+                proj_xyzd = np.full((4, 192, 256), -1,dtype=np.float32)
+                for k in range(len(real)):
+                    proj_xyzd[0, int(real[k, 4]), int(real[k, 3])] = real[k, 0]
+                    proj_xyzd[1, int(real[k, 4]), int(real[k, 3])] = real[k, 1]
+                    proj_xyzd[2, int(real[k, 4]), int(real[k, 3])] = real[k, 2]
+                    proj_xyzd[3, int(real[k, 4]), int(real[k, 3])] = real[k, 5]
+                proj_xyzd = proj_xyzd.transpose(1,2,0)
+
+                save_name = osp.join(save_path, dir_name, 'paper_cloud_data', str(i),j.split('.')[0]+'.npy')
+                np.save(save_name, proj_xyzd)
 
     # data check
-    for i in person_number :
-        org_path = osp.join(data_path,str(i))
-        org_files = os.listdir(osp.join(num_path,'attack_paper'))
-        org_files = [j for j in org_files if j.split('_')[0]=='pc']
-        
-        new_path = osp.join(save_path, 'paper_cloud_data',str(i))
-        new_files = os.listdir(new_path)
-        print(i,f' Org Data Count : {len(org_files)}  New Data Count : {len(new_files)} ==> {len(org_files)==len(new_files)}')
-        
-    
+    for dir_name in kind:
+        for i in person_number :
+            # org_path = osp.join(data_path,str(i))
+            num_path = osp.join(data_path, dir_name, str(i))
+            org_files = os.listdir(osp.join(num_path,'attack_paper'))
+            org_files = [j for j in org_files if j.split('_')[0]=='pc']
+
+            new_path = osp.join(save_path, dir_name, 'paper_cloud_data',str(i))
+            new_files = os.listdir(new_path)
+            print(i,f' Org Data Count : {len(org_files)}  New Data Count : {len(new_files)} ==> {len(org_files)==len(new_files)}')
+
